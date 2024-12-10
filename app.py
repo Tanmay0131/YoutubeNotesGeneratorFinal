e64df44b14f5e03fe7d0e073b13df31578827da3
Tanmay Sharma <tanmaysharma0131@gmail.com>
2024-12-10 12:10:41 -0500

    3rd commit

--- app.py
+++ app.py
@@ -341,4 +341,6 @@ def get_transcript_from_youtube_url(video_url):
 #     return 'File not found', 404
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
