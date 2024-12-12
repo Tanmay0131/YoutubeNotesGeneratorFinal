89f64f6939cb2d194be92f7a45fc719af9c1a4a0
Tanmay Sharma <tanmaysharma0131@gmail.com>
2024-12-12 14:18:23 -0500

    reverted version

--- app.py
+++ app.py
@@ -341,6 +341,4 @@ def get_transcript_from_youtube_url(video_url):
 #     return 'File not found', 404
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
+    app.run(debug=True, port=5501) 
