0962441c53c90f6a5b079b3f4f109981c3cdc62e
Tanmay Sharma <tanmaysharma0131@gmail.com>
2025-04-28 13:07:19 -0400

    updated commit

--- app.py
+++ app.py
@@ -41,12 +41,12 @@ def process_text():
 
         genai.configure(api_key=api_key)
 
-        try:
-            transcript = YouTubeTranscriptApi.get_transcript(youtube_link.replace("https://www.youtube.com/watch?v=", "").strip(), languages=['en'])
-            combined_text = ' '.join([item['text'] for item in transcript])
-        except:
-            transcript = get_transcript_from_youtube_url(youtube_link)
-            combined_text = transcript
+        # try:
+        transcript = YouTubeTranscriptApi.get_transcript(youtube_link.replace("https://www.youtube.com/watch?v=", "").strip(), languages=['en'])
+        combined_text = ' '.join([item['text'] for item in transcript])
+        # except:
+        #     transcript = get_transcript_from_youtube_url(youtube_link)
+        #     combined_text = transcript
 
         if "https://www.youtube.com/watch?v=" in youtube_link:
             youtube_link = youtube_link.replace("https://www.youtube.com/watch?v=", "").strip()
@@ -104,6 +104,10 @@ def process_text():
 
                 if hasattr(answers_response, 'text'):
                     answers_text = answers_response.text.replace('*', '')
+
+                    print("Notes: ", cleaned_text)
+                    print("Questions: ", questions_text)
+                    print("Answers: ", answers_text)
 
                     answers_doc = io.BytesIO()
                     answers_document = Document()
@@ -246,99 +250,99 @@ def process_text():
         return json.dumps({"error": "Failed to process. Please try again later." }), 500
     
 
-def download_YouTube_mp4(video_url):
-    yt = YouTube(video_url, on_progress_callback = on_progress)
-    video_stream = yt.streams.get_highest_resolution()
-    video_name = yt.title
-    file_path = video_stream.download()
-    return video_name, file_path
+# def download_YouTube_mp4(video_url):
+#     yt = YouTube(video_url, on_progress_callback = on_progress)
+#     video_stream = yt.streams.get_highest_resolution()
+#     video_name = yt.title
+#     file_path = video_stream.download()
+#     return video_name, file_path
 
 
-def convert_mp4_to_wav(file_path, output_path):
-    filename = os.path.splitext(os.path.basename(file_path))[0]
-    # mp.VideoFileClip.ffmpeg_binary = r"C:\\Users\\CMP_OwDiBacco\\Downloads\\Convert Youtube URL to Transcript\\ffmpeg-7.1-full_build (1)\\ffmpeg-7.1-full_build\\bin\\ffmpeg.exe" # replace with path to ffmpeg.exe after you set it as a path in your enviormental varaibles
-    # ffmpeg is not needed ^
-    video = mp.VideoFileClip(file_path)
-    output_wav_dir = os.path.join(output_path, "Wav")
-    os.makedirs(output_wav_dir, exist_ok=True)
-    output_wav_path = os.path.join(output_wav_dir, filename + '.wav')
-    video.audio.write_audiofile(output_wav_path)
-    return output_wav_path
+# def convert_mp4_to_wav(file_path, output_path):
+#     filename = os.path.splitext(os.path.basename(file_path))[0]
+#     # mp.VideoFileClip.ffmpeg_binary = r"C:\\Users\\CMP_OwDiBacco\\Downloads\\Convert Youtube URL to Transcript\\ffmpeg-7.1-full_build (1)\\ffmpeg-7.1-full_build\\bin\\ffmpeg.exe" # replace with path to ffmpeg.exe after you set it as a path in your enviormental varaibles
+#     # ffmpeg is not needed ^
+#     video = mp.VideoFileClip(file_path)
+#     output_wav_dir = os.path.join(output_path, "Wav")
+#     os.makedirs(output_wav_dir, exist_ok=True)
+#     output_wav_path = os.path.join(output_wav_dir, filename + '.wav')
+#     video.audio.write_audiofile(output_wav_path)
+#     return output_wav_path
 
 
-def convert_wav_to_text(file_path, output_path):
-    chunk_text = []
-    audio = AudioSegment.from_wav(file_path)
-    filename = os.path.splitext(os.path.basename(file_path))[0]
-    chunks = split_on_silence(audio, min_silence_len=500, silence_thresh=-50)
-    if not chunks:
-        return ""  
+# def convert_wav_to_text(file_path, output_path):
+#     chunk_text = []
+#     audio = AudioSegment.from_wav(file_path)
+#     filename = os.path.splitext(os.path.basename(file_path))[0]
+#     chunks = split_on_silence(audio, min_silence_len=500, silence_thresh=-50)
+#     if not chunks:
+#         return ""  
 
-    recognizer = sr.Recognizer()
-    output_path = os.path.join(output_path, "Clips", filename)
-    os.makedirs(output_path, exist_ok=True)
+#     recognizer = sr.Recognizer()
+#     output_path = os.path.join(output_path, "Clips", filename)
+#     os.makedirs(output_path, exist_ok=True)
 
-    for i, chunk in enumerate(chunks):
-        full_wav_path = os.path.join(output_path, f"chunk{i+1}.wav")
-        chunk.export(full_wav_path, format="wav")
+#     for i, chunk in enumerate(chunks):
+#         full_wav_path = os.path.join(output_path, f"chunk{i+1}.wav")
+#         chunk.export(full_wav_path, format="wav")
         
-        with sr.AudioFile(full_wav_path) as source:
-            audio_chunk = recognizer.record(source, duration=4)
-            try:
-                text = recognizer.recognize_google(audio_chunk)
-                chunk_text.append(text)
-            except sr.UnknownValueError:
-                print(f"Chunk {i+1}: No Speech Recognized")
-            except sr.RequestError as e:
-                print(f"Error With Google Speech Recognition API: {e}")
-            except Exception as e:
-                print(f"An Error Occurred: {e}")
+#         with sr.AudioFile(full_wav_path) as source:
+#             audio_chunk = recognizer.record(source, duration=4)
+#             try:
+#                 text = recognizer.recognize_google(audio_chunk)
+#                 chunk_text.append(text)
+#             except sr.UnknownValueError:
+#                 print(f"Chunk {i+1}: No Speech Recognized")
+#             except sr.RequestError as e:
+#                 print(f"Error With Google Speech Recognition API: {e}")
+#             except Exception as e:
+#                 print(f"An Error Occurred: {e}")
 
-    final_text = ''.join(chunk_text)
-    return final_text
+#     final_text = ''.join(chunk_text)
+#     return final_text
 
 # def write_transcript_to_file(text, video_name):
 #     with open(os.path.join(video_name + ".txt"), "w") as txt_file:
 #         txt_file.write(text)
 
 
-def delete_individual_variables(arr):
-    for a in arr:
-        del a
+# def delete_individual_variables(arr):
+#     for a in arr:
+#         del a
 
 
-def delete_created_files(workspace, output_path):
-    delete_script_path = os.path.join(workspace, 'Delete.py')
-    with open(delete_script_path, "w") as wrfile:
-        wrfile.write('import shutil\n')
-        wrfile.write(f'shutil.rmtree(r"{output_path}")\n')
-        wrfile.write('import os\n')
-        wrfile.write(f'os.remove(r"{delete_script_path}")\n')
+# def delete_created_files(workspace, output_path):
+#     delete_script_path = os.path.join(workspace, 'Delete.py')
+#     with open(delete_script_path, "w") as wrfile:
+#         wrfile.write('import shutil\n')
+#         wrfile.write(f'shutil.rmtree(r"{output_path}")\n')
+#         wrfile.write('import os\n')
+#         wrfile.write(f'os.remove(r"{delete_script_path}")\n')
 
-    os.system(f'python3 "{delete_script_path}"')
+#     os.system(f'python3 "{delete_script_path}"')
 
 
-def get_transcript_from_youtube_url(video_url):
-    output_path_id = str(uuid.uuid4())
-    output_path = f'.\\{output_path_id}'
-    current_dir = Path.cwd()
-    delete_dir = os.path.join(current_dir, output_path_id)
-    video_name, file_path = download_YouTube_mp4(video_url)
-    wav_file_path = convert_mp4_to_wav(file_path, output_path)
-    text = convert_wav_to_text(wav_file_path, output_path)
-    delete_individual_variables([file_path, wav_file_path, output_path])
-    delete_created_files(current_dir, delete_dir)
-    return text
+# def get_transcript_from_youtube_url(video_url):
+#     output_path_id = str(uuid.uuid4())
+#     output_path = f'.\\{output_path_id}'
+#     current_dir = Path.cwd()
+#     delete_dir = os.path.join(current_dir, output_path_id)
+#     video_name, file_path = download_YouTube_mp4(video_url)
+#     wav_file_path = convert_mp4_to_wav(file_path, output_path)
+#     text = convert_wav_to_text(wav_file_path, output_path)
+#     delete_individual_variables([file_path, wav_file_path, output_path])
+#     delete_created_files(current_dir, delete_dir)
+#     return text
     # write_transcript_to_file(text, video_name)
     # delete_created_files(current_dir, delete_dir)
 
-# @app.route('/download/<file_type>')
-# def download_file(file_type):
-#     if file_type in generated_files:
-#         file_obj = generated_files[file_type]
-#         file_extension = 'docx' if file_type != 'form' else 'txt'
-#         return send_file(file_obj, as_attachment=True, download_name=f'{file_type}.{file_extension}')
-#     return 'File not found', 404
+@app.route('/download/<file_type>')
+def download_file(file_type):
+    if file_type in generated_files:
+        file_obj = generated_files[file_type]
+        file_extension = 'docx' if file_type != 'form' else 'txt'
+        return send_file(file_obj, as_attachment=True, download_name=f'{file_type}.{file_extension}')
+    return 'File not found', 404
 
 if __name__ == '__main__':
     app.run(debug=True, port=5501) 
